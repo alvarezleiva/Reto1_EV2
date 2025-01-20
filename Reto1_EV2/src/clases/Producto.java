c3fbe10aa5b38ede43c60cd01523e94d1bedc981
Alice Jamir <mariaalicejamir@gmail.com>
2025-01-20 12:36:47 +0100

    class product creada

--- Reto1_EV2/src/clases/Producto.java
+++ Reto1_EV2/src/clases/Producto.java
@@ -2,4 +2,44 @@ package clases;
 
 public class Producto {
 
+	private int id;
+	private String nombre;
+	private double precio;
+	
+	public Producto() {
+		
+	}
+	
+	public Producto(int id, String nombre, double precio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
 }
