ad6e5fedd3a26e3358b3720442e3308fafda99a7
Alice Jamir <mariaalicejamir@gmail.com>
2025-01-20 12:40:43 +0100

    class empleado creada

--- Reto1_EV2/src/clases/Empleado.java
+++ Reto1_EV2/src/clases/Empleado.java
@@ -2,4 +2,53 @@ package clases;
 
 public class Empleado {
 
+	private String nombre;
+	private int anteguedad;
+	private double sueldo;
+	
+	public Empleado() {
+		
+	}
+	
+	
+	public Empleado(String nombre, int anteguedad, double sueldo) {
+		super();
+		this.nombre = nombre;
+		this.anteguedad = anteguedad;
+		this.sueldo = sueldo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getAnteguedad() {
+		return anteguedad;
+	}
+
+
+	public void setAnteguedad(int anteguedad) {
+		this.anteguedad = anteguedad;
+	}
+
+
+	public double getSueldo() {
+		return sueldo;
+	}
+
+
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+
+
+	
+	
 }
