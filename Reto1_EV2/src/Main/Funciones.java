afcde0ffee4ea6060637464605934e8d3f7b5fec
PzDarby <izliquihd@gmail.com>
2025-01-21 09:10:38 +0100

    Añadidas funcions mostrar y buscar tienda

--- Reto1_EV2/src/Main/Funciones.java
+++ Reto1_EV2/src/Main/Funciones.java
@@ -1,6 +1,7 @@
 package Main;
 
 import java.util.List;
+import java.util.Scanner;
 
 import clases.Tienda;
 
@@ -9,6 +10,28 @@ public class Funciones {
 		for (Tienda tienda : tiendas) {
 			System.out.println(tienda.toString());
 		}
+	}
+	public static Tienda buscarTienda(List<Tienda> tiendas) {
+		boolean esta=false;
+		Tienda tienda2=null;
+		Scanner sc = new Scanner(System.in);
+		int idTienda=funciones.Funciones.dimeEntero("Dime id tienda", sc);
+		for (Tienda tienda : tiendas) {
+			if (tienda.getId()==idTienda) {
+				esta=true;
+				tienda2=tienda;
+			}
+		}
+		while (!esta) {
+			idTienda=funciones.Funciones.dimeEntero("ID incorrecto, dime id tienda", sc);
+			for (Tienda tienda : tiendas) {
+				if (tienda.getId()==idTienda) {
+					esta=true;
+					tienda2=tienda;
+				}
+			}
+		}
+		return tienda2;
 	}
 	
 }
