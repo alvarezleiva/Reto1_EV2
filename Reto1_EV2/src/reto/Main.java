427e051a4876ee84b88dd68338fad2addbde559f
Alice Jamir <mariaalicejamir@gmail.com>
2025-01-21 08:51:05 +0100

    Generando 20 empleados

--- Reto1_EV2/src/reto/Main.java
+++ Reto1_EV2/src/reto/Main.java
@@ -1,5 +1,25 @@
 package reto;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import clases.Empleado;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Random r  = new Random();
+		
+		//Generando 20 empleados
+		 List<Empleado> empleados = new ArrayList<>();
+	        for (int i = 1; i < 21; i++) {
+	        	
+	           Empleado e = new Empleado();
+	           e.setNombre("Empleado "+ i);
+	           e.setAnteguedad(r.nextInt(10,101));
+	           empleados.add(e);
+	        }
+
+	}
 }
