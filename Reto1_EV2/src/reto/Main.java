e2e608efe93689a065728cf33a17f1585f4f2127
Alice Jamir <mariaalicejamir@gmail.com>
2025-01-21 08:56:45 +0100

    a

--- Reto1_EV2/src/reto/Main.java
+++ Reto1_EV2/src/reto/Main.java
@@ -7,19 +7,21 @@ import java.util.Random;
 import clases.Empleado;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Random r  = new Random();
-		
-		//Generando 20 empleados
-		 List<Empleado> empleados = new ArrayList<>();
-	        for (int i = 1; i < 21; i++) {
-	        	 
-	           Empleado e = new Empleado();
-	           e.setNombre("Empleado "+ i);
-	           e.setAnteguedad(r.nextInt(10,101));
-	           empleados.add(e);
-	        }
 
+	public static List<Empleado> generarEmplados() {
+
+		Random r = new Random();
+
+		// Generando 20 empleados
+		List<Empleado> empleados = new ArrayList<>();
+
+		for (int i = 1; i < 21; i++) {
+
+			Empleado e = new Empleado();
+			e.setNombre("Empleado " + i);
+			e.setAnteguedad(r.nextInt(10, 101));
+			empleados.add(e);
+		}
+		return empleados;
 	}
 }
