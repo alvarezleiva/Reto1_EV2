0d522614f5143dbf50ed73fcac3c2cb9b4581961
alvarezleiva <galvarezleiv@gmail.com>
2025-01-21 09:26:59 +0100

    fix

--- Reto1_EV2/src/reto/FuncionesGerman.java
+++ Reto1_EV2/src/reto/FuncionesGerman.java
@@ -1,10 +1,31 @@
 package reto;
 
+<<<<<<< Upstream, based on branch 'main' of https://github.com/alvarezleiva/Reto1_EV2.git
+=======
+import clases.Tienda;
+import funciones.Funciones;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+import clases.Cliente;
+import clases.Empleado;
+
+>>>>>>> 8597ab7 fix
 public class FuncionesGerman {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static Cliente clienteNuevo() {
+		
+		Cliente cliente = new Cliente();
+		cliente.getNombre();
+		cliente.getListaCompras();
+		
+		return cliente;
 	}
 
 }
