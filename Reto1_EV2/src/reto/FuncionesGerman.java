df69cf92da35e37ee2f3888692e1d654721c1844
alvarezleiva <galvarezleiv@gmail.com>
2025-01-21 09:46:26 +0100

    GENERAR TIENDA Y CLIENTE

--- Reto1_EV2/src/reto/FuncionesGerman.java
+++ Reto1_EV2/src/reto/FuncionesGerman.java
@@ -1,29 +1,32 @@
 package reto;
 
-<<<<<<< Upstream, based on branch 'main' of https://github.com/alvarezleiva/Reto1_EV2.git
-=======
-import clases.Tienda;
-import funciones.Funciones;
-import java.util.Random;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import clases.Cliente;
 import clases.Empleado;
+import clases.Tienda;
 
->>>>>>> 8597ab7 fix
 public class FuncionesGerman {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void generadorTiendas(List<Empleado>empleados) {
+		Random r = new Random();
+		Empleado empleado = new Empleado();
 
+		for (int i = 1; i < 6; i++) {
+			Tienda tienda = new Tienda();
+			tienda.setNombre("Tienda" + i);
+			tienda.getId();
+			tienda.setGerente(empleados.get(r.nextInt(1,empleados.size())));
+			
+			tienda.reponerProductos(r.nextInt(5,11));
+			
+		}
 	}
 	
-	public static Cliente clienteNuevo() {
-		
-		Cliente cliente = new Cliente();
-		cliente.getNombre();
-		cliente.getListaCompras();
+	public static Cliente generarCliente() {
+		Random r = new Random();
+		Cliente cliente = new Cliente("Cliente " + r.nextInt(1,100));
 		
 		return cliente;
 	}
